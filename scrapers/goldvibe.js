2fabd1682175132efaaad136eaf1b0b52fe917c3
Nick Salvatore <nmsalvatore@gmail.com>
2024-05-11 15:00:44 -0700

    All data fully extracted and formatted for db insertion

--- scrapers/goldvibe.js
+++ scrapers/goldvibe.js
@@ -1,76 +1,115 @@
 import fs from "node:fs";
 import path from "path";
 
-async function getEventData(pageNum) {
+async function getAllEvents() {
+    let pageNum = 1;
+    let nextPage;
+    let events = [];
+
+    do {
+        const data = await getPageEventData(pageNum);
+        const pageEvents = getPageEvents(data);
+        nextPage = data.pages.nextPage;
+        events = events.concat(pageEvents);
+        pageNum++;
+    } while (nextPage);
+
+    return events;
+}
+
+async function getPageEventData(pageNum) {
     const response = await fetch(
-        `https://api.eventcalendarapp.com/events?id=11497&page=${pageNum}&widgetUuid=100b29c5-2b2b-4c70-9ca7-95e6909dcc92`,
+        `https://api.eventcalendarapp.com/events?id=11497&page=${pageNum}`,
     );
     const data = response.json();
     return data;
 }
 
 function getPageEvents(data) {
-    const pageEvents = data.events;
     const events = [];
 
-    for (let pageEvent of pageEvents) {
-        const startDatetime = new Date(pageEvent.timezoneStart);
-        const endDatetime = new Date(pageEvent.timezoneEnd);
-        const description = pageEvent.longDescription;
+    for (let event of data.events) {
         events.push({
-            title: pageEvent.summary,
+            title: getTitle(event),
             venue: "Gold Vibe Kombuchary",
             city: "Grass Valley",
-            startDate: startDatetime.toLocaleDateString(),
-            startTime: startDatetime.toLocaleTimeString(),
-            endTime: endDatetime.toLocaleTimeString(),
-            admission: getAdmissionPrice(description),
-            url: pageEvent.ticketsLink,
+            startDate: getStartDate(event),
+            startTime: getStartTime(event),
+            endTime: getEndTime(event),
+            admission: getAdmissionPrice(event),
+            url: getUrl(event),
+            continuous: false,
         });
     }
 
     return events;
 }
 
-function getAdmissionPrice(text) {
-    const regex = /\$\d+/g;
-    const prices = text.match(regex);
+function getTitle(event) {
+    return event.summary;
+}
+
+function getStartDate(event) {
+    const dateString = event.timezoneStart;
+    const date = new Date(dateString);
+    date.setHours(0, 0, 0, 0);
+    date.setHours(date.getHours() - 7);
+    return date;
+}
+
+function getStartTime(event) {
+    const dateString = event.timezoneStart;
+    const time = getTime(dateString);
+    return time;
+}
+
+function getEndTime(event) {
+    const dateString = event.timezoneEnd;
+    const time = getTime(dateString);
+    return time;
+}
+
+function getTime(dateString) {
+    const date = new Date(dateString);
+    const time = date.toLocaleTimeString("en-US", { hour12: false });
+    return time;
+}
+
+function getAdmissionPrice(event) {
+    const text = event.description;
+    const prices = matchPrices(text);
 
     if (!prices) {
         return null;
     }
 
+    const [minPrice, maxPrice] = getMinMaxPrices(prices);
+    const priceString = formatPriceString(minPrice, maxPrice);
+    return priceString;
+}
+
+function formatPriceString(minPrice, maxPrice) {
+    if (minPrice === maxPrice) {
+        return `$${minPrice}`;
+    }
+    return `$${minPrice}-$${maxPrice}`;
+}
+
+function getMinMaxPrices(prices) {
     const numericPrices = prices.map((price) => Number(price.slice(1)));
     const minPrice = Math.min(...numericPrices);
     const maxPrice = Math.max(...numericPrices);
-
-    if (minPrice === maxPrice) {
-        return `$${minPrice}`;
-    } else {
-        return `$${minPrice}-$${maxPrice}`;
-    }
+    return [minPrice, maxPrice];
 }
 
-let pageNum = 1;
-let nextPage;
-let events = [];
+function matchPrices(text) {
+    const regex = /\$\d+/g;
+    const prices = text.match(regex);
+    return prices;
+}
 
-do {
-    const data = await getEventData(pageNum);
-    const pageEvents = getPageEvents(data);
-    nextPage = data.pages.nextPage;
-    events = events.concat(pageEvents);
-    pageNum++;
-} while (nextPage);
+function getUrl(event) {
+    return event.ticketsLink;
+}
 
-fs.writeFile(
-    path.join("..", "calendars", "goldvibe.json"),
-    JSON.stringify(events, null, 2),
-    (err) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log("File written succesfully");
-        }
-    },
-);
+export default getAllEvents;
