4a930bc019de91d776cc290f7ea86c4f7804e154
Nick Salvatore <nmsalvatore@gmail.com>
2024-06-24 16:09:10 -0700

    Initial commit of Golden Era scraper

---
+++ scrapers/goldenera.js
@@ -0,0 +1,90 @@
+import jsdom from "jsdom";
+import { formatTime } from "../utils/dates.js";
+
+async function getPageDocument() {
+    const url = "https://www.goldeneralounge.com/events";
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const html = await response.text();
+        const { JSDOM } = jsdom;
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        return document;
+    }
+}
+
+async function getAllEvents(sql) {
+    await sql`DELETE FROM events_event WHERE venue = 'Golden Era Lounge'`;
+
+    const events = [];
+    const document = await getPageDocument();
+    const upcomingEvents = document.querySelectorAll(
+        "article.eventlist-event--upcoming",
+    );
+
+    for (const event of upcomingEvents) {
+        const title = getTitle(event);
+        const venue = "Golden Era Lounge";
+        const city = "Nevada City";
+        const startDate = getStartDate(event);
+        const startTime = getStartTime(event);
+        const endTime = getEndTime(event);
+        const admission = null;
+        const url = getUrl(event);
+        const continuous = false;
+
+        const eventData = {
+            title,
+            venue,
+            city,
+            startDate,
+            startTime,
+            endTime,
+            admission,
+            url,
+            continuous,
+        };
+
+        events.push(eventData);
+    }
+
+    return events;
+}
+
+function getTitle(event) {
+    const title = event.querySelector("h1").textContent;
+    return title;
+}
+
+function getStartTimeElement(event) {
+    const timeElement = event.querySelector("time.event-time-localized-start");
+    return timeElement;
+}
+
+function getStartDate(event) {
+    const timeElement = getStartTimeElement(event);
+    const startDate = timeElement.getAttribute("datetime");
+    return startDate;
+}
+
+function getStartTime(event) {
+    const timeElement = getStartTimeElement(event);
+    const time = formatTime(timeElement.textContent);
+    return time;
+}
+
+function getEndTime(event) {
+    const timeElement = event.querySelector("time.event-time-localized-end");
+    const time = formatTime(timeElement.textContent);
+    return time;
+}
+
+function getUrl(event) {
+    const linkElement = event.querySelector("a.eventlist-button");
+    const basePath = "https://www.goldeneralounge.com";
+    const url = basePath + linkElement.href;
+    return url;
+}
+
+export default getAllEvents;
